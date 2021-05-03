1c98ebb55f3082d1088804acee25f97897c29427
Dave Winer <dave.winer@gmail.com>
2021-05-03 11:36:37 -0400

    v0.4.0

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # artDownloader
+
 A collection of great art in one big folder. Download and point your screen saver at the folder for a live art show.
+
+### How to use
+
+1. Download the folder.
+
+2. Unzip it.
+
+3. In your Settings app, open the Screen Saver panel, and choose the images sub-folder of the data folder. 
+
+4. When your system is in idle, enjoy an art show on your desktop. Or set it up on a screen in your living room or lobby, for an art show for your friends.
+
+### The story
+
+I had some time this weekend to do a little software project I've wanted to do for a while, it was only recently possible. 
+
+It's a growing folder of great art, for use in a screen saver, or whatever other app you might have in mind. 
+
+Bill Gates has this in his house, but i think everyone should be able to have it. You shouldn't have to be rich to have your life enriched by art. 
+
+It's possible because there's a relatively new thing on Twitter, where people have adopted famous artists, they post their great works, and retweet works of other famous artist accounts, so it's like a little social network of great art.
+
+I have an app that downloads the art from those accounts. That app, in source, is in the app folder here. The data it downloads is in the data folder, offered in both image and json formats. The images are what you should point your screen saver at. 
+
+### Running the app
+
+You can totally enjoy the art without running the app. I'm including it here so other developers who have ideas don't have to reimplement what I've already got running.
+
+The app is written in JavaScript designed to run in Node.js.
+
+Assuming you know how to run a Node app, you have to create a config.json file with four elements. 
+
+3. twitterConsumerKey
+
+4. twitterConsumerSecret
+
+1. myAccessToken
+
+2. myAccessTokenSecret
+
+The first two bits identify the Twitter app you running from. You can create an app on this page at Twitter's website.
+
+The second two bits identify you as a user. You can generate these using the same page on twitter.com.
+
+I've included an example config.json file in the folder with fake data. 
+
+If you leave the app running, it'll check every feed about once an hour and download the new images. 
+
---
+++ app/artdownloader.js
@@ -0,0 +1,206 @@
+const myVersion = "0.4.0", myProductName = "artDownloader";
+
+const fs = require ("fs");
+const utils = require ("daveutils");
+const request = require ("request");
+const davetwitter = require ("davetwitter"); 
+
+var config = {
+	mediaFilePath: "data/images/",
+	jsonFilePath: "data/json/",
+	
+	minSecsBetwChecks: 60 * 60, //1 hour
+	
+	artists: [
+		"pablocubist", "artistklee", "artistrothko", "vangoghartist", "artistmanet", 
+		"artistdegas", "cezanneart", "artistgauguin", "artistmorisot", "artist_dali", 
+		"artistchagall", "artistmatisse", "franzmarcart", "artistpollock", "artistdekooning",
+		"artfridakahlo", "artistrivera", "artistokeeffe", "artlichtenstein", "ArtistDaVinci",
+		"fanmichelangelo", "artistraphael", "artistholbein", "artistbruegel", "artistmagritte",
+		"artisthopper"
+		]
+	};
+
+var stats = {
+	ctPictures: 0,
+	whenLastPicture: undefined,
+	artists: new Object ()
+	};
+
+const fnameConfig = "config.json", fnameStats = "stats.json";
+
+var flStatsChanged = undefined;
+
+
+function statsChanged () {
+	flStatsChanged = true;
+	}
+function haveThisArt (screenname, fname) {
+	try {
+		var f = config.mediaFilePath + fname;
+		fs.statSync (f);
+		return (true);
+		}
+	catch (err) {
+		return (false);
+		}
+	}
+function addWorkOfArt (screenname, fname, theArt) {
+	var f = config.jsonFilePath + screenname + "/" + utils.stringPopExtension (fname) + ".json";
+	var jsontext = utils.jsonStringify (theArt);
+	utils.sureFilePath (f, function () {
+		fs.writeFile (f, jsontext, function (err) {
+			});
+		});
+	console.log (jsontext);
+	}
+function downloadMedia (urlMedia, fname, screenname, callback) {
+	var f = config.mediaFilePath + fname;
+	utils.sureFilePath (f, function () {
+		request (urlMedia).pipe (fs.createWriteStream (f));
+		});
+	}
+function checkOneArtist (screenname) {
+	var token = config.myAccessToken, secret = config.myAccessTokenSecret, now = new Date ();
+	console.log ("checkOneArtist: screenname == " + screenname + ", now == " + now.toLocaleTimeString ());
+	if (stats.artists [screenname] === undefined) {
+		stats.artists [screenname] = {
+			whenAdded: now,
+			whenLastCheck: undefined,
+			ctChecks: 0,
+			ctErrors: 0,
+			lastErrorMessage: undefined,
+			id: undefined,
+			idLastTweet: undefined,
+			ctPictures: 0,
+			whenLastPicture: undefined
+			};
+		}
+	var myStats = stats.artists [screenname];
+	myStats.whenLastCheck = now;
+	myStats.ctChecks++;
+	
+	function getTwitterUserId (callback) {
+		if (myStats.id !== undefined) {
+			callback (undefined, myStats.id);
+			}
+		else {
+			davetwitter.getUserInfo (token, secret, screenname, function (err, data) {
+				if (err) {
+					callback (err);
+					}
+				else {
+					myStats.id = data.id_str;
+					callback (undefined, myStats.id);
+					}
+				});
+			}
+		}
+	getTwitterUserId (function (err, id) {
+		if (!err) {
+			davetwitter.getTimeline (token, secret, "user", id, myStats.idLastTweet, function (err, theTimeline) {
+				if (err) {
+					myStats.ctErrors++;
+					myStats.lastErrorMessage = err.message;
+					}
+				else {
+					theTimeline.forEach (function (theTweet, ix) {
+						if (theTweet.retweeted_status === undefined) { //it's not a RT
+							if (theTweet.entities.media !== undefined) {
+								var mediaObject = theTweet.entities.media [0];
+								if (mediaObject !== undefined) {
+									var urlMedia = mediaObject.media_url;
+									var width = mediaObject.sizes.large.w;
+									var height = mediaObject.sizes.large.h;
+									var fname = utils.stringLastField (urlMedia, "/");
+									if (!haveThisArt (screenname, fname)) {
+										addWorkOfArt (screenname, fname, {
+											text: theTweet.full_text,
+											url: urlMedia,
+											fname,
+											w: width,
+											h: height,
+											name: screenname,
+											id: theTweet.id_str,
+											when: new Date ()
+											});
+										downloadMedia (urlMedia, fname, screenname, function (err) {
+											if (err) {
+												console.log ("error downloading file " + fname + " == " + err.message);
+												}
+											else {
+												console.log ("file " + fname + " has been downloaded");
+												}
+											});
+										myStats.ctPictures++;
+										myStats.whenLastPicture = now;
+										stats.ctPictures++;
+										stats.whenLastPicture = now;
+										}
+									}
+								}
+							}
+						myStats.idLastTweet = theTweet.id_str; 
+						});
+					statsChanged ();
+					}
+				})
+			}
+		});
+	}
+function checkNextArtist () {
+	var screenname, oldestCheck = new Date ();
+	for (var i = 0; i < config.artists.length; i++) {
+		var name = config.artists [i];
+		if (stats.artists [name] === undefined) {
+			screenname = name;
+			break;
+			}
+		else {
+			let whenLastCheck = new Date (stats.artists [name].whenLastCheck);
+			if (utils.secondsSince (whenLastCheck) >= config.minSecsBetwChecks) {
+				if (whenLastCheck < oldestCheck) {
+					oldestCheck = whenLastCheck;
+					screenname = name;
+					}
+				}
+			}
+		}
+	if (screenname !== undefined) {
+		checkOneArtist (screenname);
+		}
+	}
+function everyMinute () {
+	checkNextArtist ();
+	}
+function everySecond () {
+	if (flStatsChanged) {
+		flStatsChanged = false;
+		fs.writeFile (fnameStats, utils.jsonStringify (stats), function (err) {
+			});
+		}
+	}
+function readConfig (f, config, callback) {
+	fs.readFile (f, function (err, jsontext) {
+		if (!err) {
+			try {
+				var jstruct = JSON.parse (jsontext);
+				for (var x in jstruct) {
+					config [x] = jstruct [x];
+					}
+				}
+			catch (err) {
+				console.log ("Error reading " + f);
+				}
+			}
+		callback ();
+		});
+	}
+readConfig (fnameStats, stats, function () {
+	readConfig (fnameConfig, config, function () {
+		config.flServerEnabled = false; 
+		davetwitter.start (config);
+		utils.runEveryMinute (everyMinute);
+		setInterval (everySecond, 1000); 
+		});
+	});
